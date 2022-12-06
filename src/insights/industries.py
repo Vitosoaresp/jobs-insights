f5d250f44f2483c5f1c943399763a3abe18c03c1
Vitor Soares <pereiravitor1218@gmail.com>
2022-12-06 16:00:59 -0300

    feat[req 3]: get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,19 @@
-from typing import List, Dict
+from typing import Dict, List
+
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    jobs_data = read(path)
+    sum_jobs_by_industry = {}
+    for job in jobs_data:
+        if job['industry'] == '':
+            continue
+        elif job['industry'] in sum_jobs_by_industry:
+            sum_jobs_by_industry[job['industry']] += 1
+        else:
+            sum_jobs_by_industry[job['industry']] = 1
+    return sum_jobs_by_industry
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
