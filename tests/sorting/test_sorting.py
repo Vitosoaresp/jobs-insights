567f5f4a56e79e72994389efc1d007e86b65402d
Vitor Soares <pereiravitor1218@gmail.com>
2022-12-08 17:03:33 -0300

    lint: formata arquivo usando o black

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -8,22 +8,47 @@ fake_jobs = [
     {"max_salary": 1900, "min_salary": 100, "date_posted": "2022-12-06"},
 ]
 
+
 def test_sort_by_criteria():
     expect_results = {
         "max_salary": [
-            {"max_salary": 10000, "min_salary": 200, "date_posted": "2022-12-07"},
-            {"max_salary": 1900, "min_salary": 100, "date_posted": "2022-12-06"},  
+            {
+                "max_salary": 10000,
+                "min_salary": 200,
+                "date_posted": "2022-12-07",
+            },
+            {
+                "max_salary": 1900,
+                "min_salary": 100,
+                "date_posted": "2022-12-06",
+            },
             {"max_salary": 1500, "min_salary": 0, "date_posted": "2022-12-08"},
         ],
         "min_salary": [
             {"max_salary": 1500, "min_salary": 0, "date_posted": "2022-12-08"},
-            {"max_salary": 1900, "min_salary": 100, "date_posted": "2022-12-06"},
-            {"max_salary": 10000, "min_salary": 200, "date_posted": "2022-12-07"},
+            {
+                "max_salary": 1900,
+                "min_salary": 100,
+                "date_posted": "2022-12-06",
+            },
+            {
+                "max_salary": 10000,
+                "min_salary": 200,
+                "date_posted": "2022-12-07",
+            },
         ],
         "date_posted": [
             {"max_salary": 1500, "min_salary": 0, "date_posted": "2022-12-08"},
-            {"max_salary": 10000, "min_salary": 200, "date_posted": "2022-12-07"},
-            {"max_salary": 1900, "min_salary": 100, "date_posted": "2022-12-06"},  
+            {
+                "max_salary": 10000,
+                "min_salary": 200,
+                "date_posted": "2022-12-07",
+            },
+            {
+                "max_salary": 1900,
+                "min_salary": 100,
+                "date_posted": "2022-12-06",
+            },
         ],
     }
 
@@ -32,5 +57,7 @@ def test_sort_by_criteria():
         sort_by(fake_jobs, keys)
         assert fake_jobs == expect_results[keys]
 
-    with pytest.raises(ValueError, match="invalid sorting criteria: chave errada"):
+    with pytest.raises(
+        ValueError, match="invalid sorting criteria: chave errada"
+    ):
         sort_by(fake_jobs, "chave errada")
