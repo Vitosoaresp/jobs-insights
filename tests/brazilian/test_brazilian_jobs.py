8016780d092290ae52293c84e2bfaeeb7fd21fed
Vitor Soares <pereiravitor1218@gmail.com>
2022-12-08 16:25:14 -0300

    test: cria test para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,22 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+import pytest
+
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    expected_keys = ["title", "salary", "type"]
+
+    result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert type(result) == list
+    assert len(result) == 15
+
+    for job in result:
+        assert type(job) == dict
+        for keys in expected_keys:
+            assert keys in job
+            assert "titulo" not in job
+            assert "salario" not in job
+            assert "tipo" not in job
+    
+    with pytest.raises(FileNotFoundError):
+        read_brazilian_file("um_arquivo_que_não_existe.csv")
