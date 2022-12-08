d3725e5ca5ca9524b8677308df122f6cf7c5d5c1
Vitor Soares <pereiravitor1218@gmail.com>
2022-12-08 15:46:38 -0300

    test: cria test para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,19 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    expected_counter_list = [122, 1639]
+    results = count_ocurrences("data/jobs.csv", 'javascript')
+    assert type(results) is int
+    assert results == expected_counter_list[0]
+
+    results = count_ocurrences("data/jobs.csv", 'JAVASCRIPT')
+    assert results == expected_counter_list[0]
+
+    results = count_ocurrences("data/jobs.csv", 'python')
+    assert results == expected_counter_list[1]
+
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("um_arquivo_que_não_existe.csv", 'word')
